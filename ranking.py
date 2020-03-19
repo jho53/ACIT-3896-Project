88fb297f1b4b50f06967e36948a5675c4e61545c
JIAJUNATBCIT <49431626+JIAJUNATBCIT@users.noreply.github.com>
2020-03-18 21:33:40 -0700

    update ranking method

--- ranking.py
+++ ranking.py
@@ -1,5 +1,4 @@
 import json
-import csv
 
 # pref in Json file is follow format:
 # [{"location": {"like": "", "disklike": ""}}, {"time": {"like": "xx,xx,xx", "dislike": "xx"}}]
@@ -7,43 +6,67 @@ import csv
 
 
 # Check if instructor has any preference
-def has_pref(ins_id, ins_data):
-    return ins_data[ins_id]["pref"] is not None and len(ins_data[ins_id]["pref"]) > 0
+def has_pref(ins_id, ins_json):
+    return ins_json[ins_id]["pref"] is not None and len(ins_json[ins_id]["pref"]) > 0
 
-#
+
+# Get instructor time table
+def get_inst_timetable(ins_id, ins_course_table):
+    return ins_course_table[ins_id]
+
+
+# Get instructor pref by id
+def get_inst_pref_by_id(ins_id, instructors):
+    return instructors[ins_id]["pref"]
+
+
 # Match pref with current row in the csv file
-def match_pref(row, ins_data, score):
+def match_pref(instructors, ins_course_table, score):
+    for instructor_id in ins_course_table:
+        if has_pref(instructor_id, instructors):
+            ins_timetable = get_inst_timetable(instructor_id, ins_course_table)
+            for segment in ins_timetable:
+                ins_pref = get_inst_pref_by_id(instructor_id, instructors)
+                # match location - like
+                # segment[2] - room in the time table
+                # segment[2][0] - campus
+                if ins_pref[0]["location"]["like"] == segment[2][0]:
+                    score += 1
+                # match location - dislike
+                if ins_pref[0]["location"]["dislike"] == segment[2][0]:
+                    score -= 1
+
+                # match time block - like
+                time_str = str(segment[3]) + str(segment[4])
+                if time_str in ins_pref[1]["time"]["like"]:
+                    score += 1
+
+                # match time block - dislike
+                if time_str in ins_pref[1]["time"]["dislike"]:
+                    score -= 1
 
     return score
 
 
 # Ranking the time table
 def score_time_table():
+
+    # initial score
     score = 0
+
     # Load Instructors
-    with open("ins_file.json", "r") as data:
-        ins_data = json.load(data)
+    with open("ins_file.json", "r") as ins_data:
+        instructors = json.load(ins_data)
 
-    time_table = []
     # Load time table
-    with open('new_timetable.csv', "r") as csvfile:
-        csv_data = csv.reader(csvfile, delimiter=',')
-        for course in csv_data:
-            time_table.append(course)
+    with open('ins_course.json', "r") as timeTable_data:
+        ins_course_table = json.load(timeTable_data)
 
-    for ins_id in ins_data:
-        ins_course = []
-        if has_pref(ins_id, ins_data):
-            for course in time_table:
-                if course[1] == ins_id:
-                    ins_course.append(course)
-            for i in ins_course:
-                print(i)
-            print('-----------------')
-
+    # Match preferences
+    score = match_pref(instructors, ins_course_table, score)
 
     # Output score
-    print("total score is: " + str(score))
+    print("Total score of the time table is: " + str(score))
 
 
 def main():
